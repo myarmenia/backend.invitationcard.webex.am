957d4c48c754cadc812d04acddd2f2757033f952
NarineHay <naromisho87@gmail.com>
2024-06-20 16:45:58 +0400

    20.06. test1 111

--- database/migrations/2024_05_19_093800_create_tariffs_table.php
+++ database/migrations/2024_05_19_093800_create_tariffs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('price');
             $table->integer('month');
-            $table->integer('type');
+            $table->string('type');
             $table->boolean('status')->default(1);
             $table->timestamps();
         });
